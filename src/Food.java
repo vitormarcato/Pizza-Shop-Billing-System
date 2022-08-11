991f997c6f7c3392b4dcc9e60e436f831f742961
Vitor Marcato <vitor.marcatof@gmail.com>
2022-08-10 22:17:14 -0300

    testing classes

---
+++ src/Food.java
@@ -0,0 +1,33 @@
+
+public abstract class Food {
+	
+	private int id;
+	private String name;
+	private double price;
+	private String type;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
